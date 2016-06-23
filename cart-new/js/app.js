b7bef246801b9c589c8f8c14bb4466e144ce7d5a
Kshree <kavyashri.bj@gmail.com>
2016-06-23 17:19:38 +0000

    details page added
    details page added

--- cart-new/js/app.js
+++ cart-new/js/app.js
@@ -2,9 +2,17 @@ var myApp=angular.module('shoppingApp',['ngRoute','listApp']);
 
 myApp.config(function($routeProvider) {
 	 $routeProvider
-	 .when('/details', {
-        templateUrl: 'details.html'
+	 .when('/', {
+        templateUrl: 'list.html',
+        controller: 'listCtrl'
     })
+	 .when('/details/:itemId',{
+	 	templateUrl: 'details.html',
+	 	controller: 'detailsCtrl'
+	 })
+	 .when('/checkout',{
+	 	templateUrl: 'checkout.html'
+	 })
 	.otherwise({
         templateUrl: 'list.html',
         controller: 'listCtrl'
